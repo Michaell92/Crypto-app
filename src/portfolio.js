5455ddf025bcd98131190f8e956c9b3721d774d2
Mike <underthebed24@gmail.com>
2022-04-28 05:51:27 +0200

    added portfolio calc

--- src/portfolio.js
+++ src/portfolio.js
@@ -8,7 +8,8 @@ const globalLink = 'https://api.coingecko.com/api/v3/global';
 
 const portfolio = document.getElementById('coins');
 const lsCoins = JSON.parse(localStorage.getItem('portfolio'));
-const results = document.getElementById('results');
+const total = document.getElementById('total');
+let inputValue = '';
 
 // Event listeners
 document.addEventListener('DOMContentLoaded', () => {
@@ -28,34 +29,74 @@ portfolio.addEventListener('click', async (e) => {
   const back = e.target.closest('.back');
   const del = e.target.closest('.delete');
   const coin = e.target.closest('.name');
+  const edit = e.target.closest('.edit-quantity');
+  const confirm = e.target.closest('.confirm-quantity');
 
-  // Return home
-  if (back) location.href = '/index.html';
-
-  // Get coin details
-  if (coin) {
-    getCoinDetails(coin);
-  }
-
-  // Remove coin from portfolio
-  if (del) {
-    const row = del.closest('.row');
-    const id = row.id;
-
-    // Remove coin from dom
-    row.remove();
-
-    // Remove coin from ls
-    const lsCoins = localStorage.getItem('portfolio');
-    const coins = JSON.parse(lsCoins);
-    coins.splice(coins.indexOf(id), 1);
-    localStorage.setItem('portfolio', JSON.stringify(coins));
-
-    // Check if portfolio is empty
-    const allCoins = JSON.parse(localStorage.getItem('portfolio'));
-    if (!allCoins || !allCoins.length) setMessage();
+  switch (true) {
+    case !!back:
+      location.href = '/index.html';
+      break;
+    case !!coin:
+      getCoinDetails(coin);
+      break;
+    case !!edit:
+      const row = e.target.closest('.row');
+      editQuantity(row);
+      break;
+    case !!confirm:
+      confirmQuantity(e.target.closest('.row'));
+      break;
+    case !!del:
+      deleteCoin(del);
+      break;
   }
 });
+
+function editQuantity(row) {
+  const input = row.querySelector('.input-quantity');
+  const quantity = row.querySelector('.quantity');
+
+  toggleVisibility(row, quantity, input);
+
+  // Track input value
+  input.addEventListener('input', trackChanges);
+  input.focus();
+  // Show current value
+  input.value = quantity.innerText;
+
+  // Reset coin value
+  inputValue = quantity.innerText;
+}
+
+function confirmQuantity(row) {
+  const quantity = row.querySelector('.quantity');
+  const input = row.querySelector('.input-quantity');
+
+  input.removeEventListener('input', trackChanges);
+
+  toggleVisibility(row, quantity, input);
+
+  // Show new value
+  quantity.innerText = inputValue;
+
+  calculateTotal();
+}
+
+function toggleVisibility(row, quantity, input) {
+  const confirm = row.querySelector('.confirm-quantity');
+  const edit = row.querySelector('.edit-quantity');
+
+  quantity.classList.toggle('hide-element');
+  edit.classList.toggle('hide-element');
+  input.classList.toggle('show-element');
+  confirm.classList.toggle('show-element');
+}
+
+function trackChanges(e) {
+  const newValue = e.target.value;
+
+  if (isNumber(newValue)) inputValue = newValue;
+}
 
 async function getPortfolioCoins() {
   // Set loader
@@ -108,6 +149,7 @@ function showMessage() {
   return message;
 }
 
+// Add coins to HTML
 function addToHTML(coins) {
   let html = '';
   const coinsHTML = document.getElementById('coins');
@@ -116,17 +158,27 @@ function addToHTML(coins) {
     html += `
     <div class="row" id=${coin.id}>
           <img src="${coin.image.replace('large', 'thumb')}" class="coin">
-          <span><span class="name">${coin.name}</span></span>
-          <span class="symbol">${coin.symbol}</span>
-          <span>$<span class="price">${formatNumber(
+          <span class="cell"><span class="name">${coin.name}</span></span>
+          <span class="symbol cell">${coin.symbol}</span>
+          <span class="cell">$<span class="price">${formatNumber(
             coin.current_price
           )}</span></span>
-          <span class="percent">${formatPercent(
+          <span class="percent cell">${formatPercent(
             coin.price_change_percentage_24h
           )}</span>
-
-          <span class="quantity">1</span>
-          <div class="tooltip" data-port="Remove from portfolio"><img class="delete" src="./img/delete.png" alt=""></div>
+          <div class="cell edit-container">
+          <div class="total cell">
+              <span class="quantity">1</span>
+              <input type="text" class="input-quantity">
+          </div>
+            <div class="quantity-container" data-port="Change quantity">
+              <i class="fas fa-pencil-alt edit-quantity"></i>
+              <i class="fas fa-check confirm-quantity"></i>
+            </div>
+          </div>
+          <div class="tooltip" data-port="Remove from portfolio">
+              <img class="delete" src="./img/delete.png" alt="">
+          </div>
     </div>
     `;
   }
@@ -134,9 +186,31 @@ function addToHTML(coins) {
   coinsHTML.innerHTML = html;
 }
 
+// Delete a coin from the portfolio
+function deleteCoin(del) {
+  const row = del.closest('.row');
+  const id = row.id;
+
+  // Remove coin from dom
+  row.remove();
+
+  // Remove coin from ls
+  const lsCoins = localStorage.getItem('portfolio');
+  const coins = JSON.parse(lsCoins);
+  coins.splice(coins.indexOf(id), 1);
+  localStorage.setItem('portfolio', JSON.stringify(coins));
+
+  // Check if portfolio is empty
+  const allCoins = JSON.parse(localStorage.getItem('portfolio'));
+  if (!allCoins || !allCoins.length) setMessage();
+
+  // Recalculate total
+  calculateTotal();
+}
+
 // Calculate total portfolio value
 function calculateTotal() {
-  let total = 0;
+  let sum = 0;
 
   const rows = document.querySelector('#coins').querySelectorAll('.row');
 
@@ -144,11 +218,10 @@ function calculateTotal() {
     const value =
       parseFloat(rows[i].querySelector('.price').innerHTML.replace(',', '')) *
       rows[i].querySelector('.quantity').innerHTML;
-    total += value;
-    console.log(value);
+    sum += value;
   }
 
-  results.innerHTML = `Total: $${formatNumber(total)}`;
+  total.innerHTML = formatNumber(sum);
 }
 
 async function getCoinDetails(coin) {
@@ -164,6 +237,6 @@ async function getCoinDetails(coin) {
   document.body.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
 
-`
-            <span class="title">Total:</span>
-            `;
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && !isNaN(n - 0);
+}
